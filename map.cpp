abbd67c262af7dee4a12f0caa1f8bd133d278517
Taej-P <78579124+Taej-P@users.noreply.github.com>
2022-06-16 19:48:13 +0900

    add static map

--- map.cpp
+++ map.cpp
@@ -8,39 +8,40 @@ MAP::MAP(int size) : map_size(size) {
   MAP_init();
 }
 
-void MAP::MAP_init() {
+MAP::~MAP()
+{
   for(int i = 0; i < map_size; i++)
   {
-    for(int j = 0; j < map_size; j++)
+    delete[] m[i];
+  }
+
+  delete[] m;
+
+  m = nullptr;
+}
+
+void MAP::MAP_init(int index) {
+  
+    for(int i = 0; i < map_size; i++)
     {
-      if(i == 0 || i == map_size - 1)
+      for(int j = 0; j < map_size; j++)
       {
-        if(j == 0 || j == map_size - 1)
-        {
-          m[i][j] = Immune_Wall;
-        }
+        if(index == 1)
+          m[i][j] = MAP_heart[i][j];
+
+        else if(index == 2)
+          m[i][j] = MAP_smile[i][j];
+
+        else if(index == 3)
+          m[i][j] = MAP_cross_heart[i][j];
 
         else
-        {
-          m[i][j] = Wall;
-        }
-      }
-
-      else if (j == 0 || j == map_size - 1)
-      {
-        m[i][j] = Wall;
-      }
-
-      else
-      {
-        m[i][j] = BackGround;
+          m[i][j] = MAP_basic[i][j];
       }
     }
-  }
 }
 
 void MAP::MAP_print(WINDOW* board) const {
-  wmove(board, 1, 1);
   for(int i = 0;  i < map_size; i++)
   {
     for(int j = 0; j < map_size; j++)
