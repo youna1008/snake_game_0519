0471e631b05623cb81b2982b486f281164bf71b3
Taej-P <78579124+Taej-P@users.noreply.github.com>
2022-06-16 19:49:46 +0900

    add enter gate

--- snake.cpp
+++ snake.cpp
@@ -2,19 +2,20 @@
 
 void SNAKE::SNAKE_init(MAP& map)
 {
-  for(int i = 0; i < 10; i++)
+  snake_size = 3;
+  for(int i = 0; i < snake_size; i++)
   {
     s[i].POINT_set(map.MAP_getMapSize() >> 1, (map.MAP_getMapSize() >> 1) - 1 + i);
   }
   SNAKE_insertMAP(map);
 }
 
-void SNAKE::SNAKE_move(MAP& map, int KEY_input)
+void SNAKE::SNAKE_move(int KEY_input)
 {
   for(int i = 9; i > 0; i--)
-    {
-      s[i] = s[i - 1];
-    }
+  {
+    s[i] = s[i - 1];
+  }
 
   if(KEY_input == int(KEY_UP))
   {
@@ -31,6 +32,62 @@ void SNAKE::SNAKE_move(MAP& map, int KEY_input)
   if(KEY_input == int(KEY_RIGHT))
   {
     s[0].POINT_set(s[0].POINT_getX(), s[0].POINT_getY() + 1);
+  }
+}
+
+void SNAKE::SNAKE_EatItem(MAP& map, ITEM* &item) // if item is growth, snake_size++. if item poison, snake_size--
+{
+  POINT temp = s[0];
+  int val = map.MAP_getValue(temp);
+  switch (val)
+  {
+    case Growth :
+      snake_size += 1;
+      break;
+    case Poison :
+      snake_size -= 1;
+      break;
+  }
+
+  int i = 0;
+  for(i = 0; i < 3; i++)
+  {
+    if(item[i].ITEM_getPoint() == s[0])
+    {
+      item[i].ITEM_delete(map);
+      break;
+    }
+  }
+}
+
+void SNAKE::SNAKE_EnterGate(MAP& map, GATE& g, int& direction)
+{
+  s[0] = g.GATE_returnPAIR(s[0]);
+  g.GATE_flaginc();
+
+  int key_set[4] = {KEY_UP, KEY_RIGHT, KEY_DOWN, KEY_LEFT};
+  POINT key_for_move[4] = {POINT(-1, 0), POINT(0, 1), POINT(1, 0), POINT(0, -1)};
+
+  int start_key_pos = 0;
+
+  for(int i = 0; i < 4; i++)
+  {
+    if(key_set[i] == direction)
+    {
+      start_key_pos = i;
+      break;
+    }
+  }
+
+  for(int i = 0; i < 4; i++)
+  {
+    POINT temp = s[0] + key_for_move[(start_key_pos + i) % 4];
+    if(temp.POINT_isin_range(map.MAP_getMapSize()) && (map.MAP_getValue(temp) != Wall))
+    {
+      s[0] = temp;
+      direction = key_set[(start_key_pos + i) % 4];
+      break;
+    }
   }
 }
 
@@ -63,29 +120,4 @@ void SNAKE::SNAKE_insertMAP(MAP& map)
       map.MAP_set(s[i], Snake_Body);
   }
   map.MAP_set(s[0], Snake_Head);
-}
-
-void SNAKE::SNAKE_EatItem(MAP& map, ITEM* &item) // if item is growth, snake_size++. if item poison, snake_size--
-{
-  POINT temp = s[0];
-  int val = map.MAP_getValue(temp);
-  switch (val)
-  {
-    case Growth :
-      snake_size += 1;
-      break;
-    case Poison :
-      snake_size -= 1;
-      break;
-  }
-
-  int i = 0;
-  for(i = 0; i < 3; i++)
-  {
-    if(item[i].ITEM_getPoint() == s[0])
-    {
-      item[i].ITEM_delete(map);
-      break;
-    }
-  }
 }
--- snake.h
+++ snake.h
@@ -6,6 +6,7 @@
 #include "point.h"
 #include "map.h"
 #include "item.h"
+#include "gate.h"
 
 using namespace std;
 
@@ -19,11 +20,11 @@ class SNAKE{
   void SNAKE_setSnakeSize(int value) { snake_size = value; }
   POINT SNAKE_getNthPoint(int idx) { return s[idx]; }
   void SNAKE_init(MAP& map);
-  void SNAKE_move(MAP& map, int KEY_input);
+  void SNAKE_move(int KEY_input);
+  void SNAKE_EatItem(MAP& map, ITEM* &item); // if item is growth, snake_size++. if item poison, snake_size--
+  void SNAKE_EnterGate(MAP& map, GATE& g, int& direction);
   void SNAKE_insertMAP(MAP& map);
   bool SNAKE_failure(MAP& map);
-
-  void SNAKE_EatItem(MAP& map, ITEM* &item); // if item is growth, snake_size++. if item poison, snake_size--
 };
 
 #endif
