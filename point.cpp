c019cad0abf6c40fb5e19c3699d34ea9e751d927
Taej-P <78579124+Taej-P@users.noreply.github.com>
2022-06-16 19:49:22 +0900

    add point add and isin range

--- point.cpp
+++ point.cpp
@@ -1,11 +1,8 @@
 #include "point.h"
 
-POINT POINT::POINT_rand(int map_size)
+POINT operator+(const POINT& p1, const POINT& p2)
 {
-  POINT t;
-  t.POINT_set(rand() % map_size, rand() % map_size);
-
-  return t;
+  return POINT(p1.x + p2.x, p1.y + p2.y);
 }
 
 POINT& POINT::operator=(const POINT& p)
@@ -21,3 +18,10 @@ bool POINT::operator==(const POINT& p) const
   return ((x == p.x) && (y == p.y));
 }
 
+POINT POINT::POINT_rand(int map_size)
+{
+  POINT t;
+  t.POINT_set(rand() % map_size, rand() % map_size);
+
+  return t;
+}
--- point.h
+++ point.h
@@ -1,6 +1,7 @@
 #ifndef _POINT_H_
 #define _POINT_H_
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -14,10 +15,11 @@ class POINT{
   void POINT_set(int coor_x = 0, int coor_y = 0)  { x = coor_x; y = coor_y; }
   int POINT_getX() { return x; }
   int POINT_getY() { return y; }
-
+  bool POINT_isin_range(int range) const { return ((x > 0) && (x < range - 1)) && ((y > 0) && (y < range - 1)); }
   static POINT POINT_rand(int map_size);
 
   //operators
+  friend POINT operator+(const POINT& p1, const POINT& p2);
   POINT& operator=(const POINT& p);
   bool operator==(const POINT& p) const;
 };
